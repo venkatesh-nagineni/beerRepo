6b45e9fb2a31174311ae490642619abc2c21ae22
Venkatesh <venky.chowdary91@gmail.com>
2019-05-11 14:21:55 +0200

    first commit

---
+++ proxyServer.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const router = express.Router();
+const request = require('request-promise');
+
+app.use(cors());
+app.use('/api', router);
+
+const port = 3001;
+
+app.listen(port, (err) => {
+    if (err) {
+      return new Error;
+    }else{
+        console.log(`server listening on ${port}`);
+    }
+  })
+
+  router.get('/beerList', (req, res) => {
+  const brewerryKey = '66f9baa6447b4d9bebdd502394ea7349';
+  const brewerryUrl = `https://sandbox-api.brewerydb.com/v2/beers?key=${brewerryKey}`;
+  request(brewerryUrl).then((data) => {
+    res.send({success: true, data: data})
+  })
+  .catch((err) => {
+      res.send({success: false, error: err});
+    });
+});
